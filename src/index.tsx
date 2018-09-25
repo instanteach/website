17db1854e82359e4a06bbbfef70d80004537641e
Bruno Wilkinson <brunowilkinson@Brunos-MacBook-Pro.local>
2018-09-25 10:08:10 +0200

    feat: add theme color palette to material-ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import { RawTheme } from './config/theme'
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+const theme = createMuiTheme(RawTheme);
+
 ReactDOM.render(
-  <App />,
+  <MuiThemeProvider theme={theme}>
+    <App />
+  </MuiThemeProvider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
