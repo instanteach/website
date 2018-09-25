92a626dea2eae365ab17e54e25a99978c56a1239
Bruno Wilkinson <brunowilkinson@Brunos-MacBook-Pro.local>
2018-09-25 10:18:49 +0200

    feat: add CssBaseline

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,3 +1,4 @@
+import CssBaseline from '@material-ui/core/CssBaseline';
 import * as React from 'react';
 import { Route, Switch } from 'react-router'
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -5,11 +6,14 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from '../App'
 
 const Routes = (props: any) => (
-  <Router {...props}>
+  <React.Fragment>
+    <CssBaseline />
+    <Router {...props}>
     <Switch>
       <Route exact={true} path="/" component={App} />
     </Switch>
   </Router>
+  </React.Fragment>
 );
 
 export default Routes;
