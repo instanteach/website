cd57ebe2d4dfd2569239ff801cc8f0286e33e20a
Bruno Wilkinson <brunowilkinson@Brunos-MacBook-Pro.local>
2018-09-29 14:13:20 +0200

    feat: material generator done

--- src/components/MaterialGenerator.tsx
+++ src/components/MaterialGenerator.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react'
+import styled from 'styled-components'
+
+const GoogleForm = styled('iframe')`
+  border: none;
+  padding-bottom: 100px;
+`
 
 class MaterialGenerator extends React.PureComponent {
   public render() {
-    return <div>
-      material
-    </div>
+    return <GoogleForm
+    src="https://docs.google.com/forms/d/e/1FAIpQLScBhRXAHDeXw8BupK1LwOGwPeK3PhZ4HJmypVaOJWaXv__0gA/viewform?embedded=true"
+    width="100%"
+    height="100%">
+    Loading...
+  </GoogleForm>
   }
 }
 
