37d402ed126437c9c53e7c3ebe50ad6854a11696
Bruno Wilkinson <brunowilkinson@Brunos-MacBook-Pro.local>
2018-09-29 14:29:08 +0200

    fat: remove App component + Contact done

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -4,6 +4,7 @@ import { Switch } from 'react-router'
 import { BrowserRouter as Router } from 'react-router-dom';
 import DefaultLayout from '../components/DefaultLayout'
 
+import Contact from '../components/Contact'
 import Home from '../components/Home'
 import MaterialGenerator from '../components/MaterialGenerator'
 
@@ -14,6 +15,7 @@ const Routes = (props: any) => (
     <Switch>
       <DefaultLayout exact={true} path="/" component={<Home />} />
       <DefaultLayout exact={true} path="/material-generator" component={<MaterialGenerator />} />
+      <DefaultLayout exact={true} path="/contact" component={<Contact />} />
     </Switch>
   </Router>
   </React.Fragment>
