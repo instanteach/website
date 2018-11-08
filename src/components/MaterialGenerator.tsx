6f4d420ccc2dc373ea4d3155ce6f1804b9c3442f
Jose Alvarado <jose_alvarado17@hotmail.com>
2018-11-07 21:10:07 -0400

    Replace form in MaterialGenerator.tsx

--- src/components/MaterialGenerator.tsx
+++ src/components/MaterialGenerator.tsx
@@ -1,19 +1,20 @@
 import * as React from 'react'
 import styled from 'styled-components'
 
-const GoogleForm = styled('iframe')`
+const TellForm = styled('iframe')`
   border: none;
-  padding-bottom: 100px;
+  padding-bottom: 20px;
+  width: 100%;
+  height: 90%
 `
 
 class MaterialGenerator extends React.PureComponent {
   public render() {
-    return <GoogleForm
-    src="https://docs.google.com/forms/d/e/1FAIpQLScBhRXAHDeXw8BupK1LwOGwPeK3PhZ4HJmypVaOJWaXv__0gA/viewform?embedded=true"
-    width="100%"
-    height="100%">
-    Loading...
-  </GoogleForm>
+    return (
+      <TellForm id="iframe" src="https://instanteach.tellform.com/#!/forms/5bcb4c11b0fd8a6456f09146">
+        Loading..
+      </TellForm>
+    )
   }
 }
 
