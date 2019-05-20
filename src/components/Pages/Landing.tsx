a2aa59f9d6186f5291b63a3fea9c5fc8053c9a2d
Jose Alvarado <jose_alvarado17@hotmail.com>
2019-05-20 04:13:35 -0400

    Fix Landing

--- src/components/Pages/Landing.tsx
+++ src/components/Pages/Landing.tsx
@@ -34,33 +34,33 @@ const LandingOption = styled(Link)`
 
 const Landing = () => (
 	<Wrapper>
-		<Grid container={true} spacing={16} justify="center">
-			<Grid item={true} xs={12} md={4} justify="center" alignItems="center" spacing={32}>
+		<Grid container={true} spacing={32} justify="center">
+			<Grid container={true} item={true} xs={12} md={4} justify="center" alignContent="center" spacing={16}>
 				<LandingOption to="/home">
 					<Typography variant="display2" align="center">Home</Typography>
 				</LandingOption>
 			</Grid>
-			<Grid item={true} xs={12} md={4} justify="center" alignItems="center" spacing={32}>
+			<Grid container={true} item={true} xs={12} md={4} justify="center" alignContent="center" spacing={16}>
 				<LandingOption to="/my-students">
 					<Typography variant="display2" align="center">My Students</Typography>
 				</LandingOption>
 			</Grid>
-			<Grid item={true} xs={12} md={4} justify="center" alignItems="center" spacing={32}>
+			<Grid container={true} item={true} xs={12} md={4} justify="center" alignContent="center" spacing={16}>
 				<LandingOption to="/material-generator">
 					<Typography variant="display2" align="center">Material Generator</Typography>
 				</LandingOption>
 			</Grid>
-			<Grid item={true} xs={12} md={4} justify="center" alignItems="center" spacing={32}>
+			<Grid container={true} item={true} xs={12} md={4} justify="center" alignContent="center" spacing={16}>
 				<LandingOption to="/lesson-plans">
 					<Typography variant="display2" align="center">Lesson Plans</Typography>
 				</LandingOption>
 			</Grid>
-			<Grid item={true} xs={12} md={4} justify="center" alignItems="center" spacing={32}>
+			<Grid container={true} item={true} xs={12} md={4} justify="center" alignContent="center" spacing={16}>
 				<LandingOption to="/about-us">
 					<Typography variant="display2" align="center">About</Typography>
 				</LandingOption>
 			</Grid>
-			<Grid item={true} xs={12} md={4} justify="center" alignItems="center" spacing={32}>
+			<Grid container={true} item={true} xs={12} md={4} justify="center" alignContent="center" spacing={16}>
 				<LandingOption to="/contact">
 					<Typography variant="display2" align="center">Contact us</Typography>
 				</LandingOption>
