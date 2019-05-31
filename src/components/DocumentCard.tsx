6aa019a60d48466b1bfdad8838a893280164b8e7
Jose Alvarado <jose_alvarado17@hotmail.com>
2019-05-31 02:32:03 -0400

    Fix DocumentCard

--- src/components/DocumentCard.tsx
+++ src/components/DocumentCard.tsx
@@ -238,7 +238,7 @@ class DocumentCard extends React.Component<IProps, IState> {
 										{
 										<CardTypeFile style={
 										(type === 'doc' || type === 'docx')
-											? { backgroundColor: '#1565C0' }
+											? { backgroundColor: '#1565C0', color: 'white' }
 											: (type === 'jpg' || type === 'jpeg' || type === 'png')
 												? { backgroundColor: '#E0E0E0', color: '#888' }
 												: (type === 'pptx')
